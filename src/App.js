d48fa0047b1081e2591b989707df5181d70b1d57
Pratik Mane <pratik.mane1299@gmail.com>
2022-03-30 19:40:31 +0530

    Setup tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className="w-full bg-slate-50 h-screen flex flex-col items-center">
+      <header className="px-20 py-3 w-full h-18 flex justify-between items-center bg-slate-100 shadow-sm">
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          className="flex justify-between items-center"
+          href="https://redux-saga.js.org/"
+          rel="nofollow noopener noreferrer"
           target="_blank"
-          rel="noopener noreferrer"
         >
-          Learn React
+          <img
+            alt="Redux Saga"
+            src="https://redux-saga.js.org/img/Redux-Saga-Logo.png"
+            className="w-15 h-10"
+          />
+          <span className="ml-4 text-2xl text-gray-800 font-normal">
+            Redux Saga Crud App
+          </span>
         </a>
+        <div>
+          <a
+            className="text-lg text-gray-800"
+            href="https://github.com/pratikmane1299/react-redux-saga-crud-app/"
+          >
+            GitHub
+          </a>
+        </div>
       </header>
+      <main className="px-20 py-4 w-full h-full flex flex-col justify-center items-center">
+        <p className="text-3xl">Crud App with react, redux and redux saga.</p>
+      </main>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  darkMode: 'media',
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
