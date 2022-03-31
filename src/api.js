c8898f2fb970b3b2f644f5b403c70d0413275ddc
Pratik Mane <pratik.mane1299@gmail.com>
2022-03-31 22:39:37 +0530

    use https in api

--- src/api.js
+++ src/api.js
@@ -2,19 +2,19 @@ import axios from "axios";
 
 export function fetchPostsApi() {
   return axios
-    .get("http://jsonplaceholder.typicode.com/posts")
+    .get("https://jsonplaceholder.typicode.com/posts")
     .then((res) => res.data);
 }
 
 export function fetchPostApi(postId) {
   return axios
-    .get("http://jsonplaceholder.typicode.com/posts/"+postId)
+    .get("https://jsonplaceholder.typicode.com/posts/"+postId)
     .then((res) => res.data);
 }
 
 export function fetchComments(postId) {
   return axios
-    .get(`http://jsonplaceholder.typicode.com/posts/${postId}/comments`)
+    .get(`https://jsonplaceholder.typicode.com/posts/${postId}/comments`)
     .then((res) => res.data);
 }
 
@@ -24,16 +24,16 @@ export function fetchUserApi(userId) {
 }
 
 export function deletePostApi(postId) {
-  return axios.delete(`http://jsonplaceholder.typicode.com/posts/${postId}`)
+  return axios.delete(`https://jsonplaceholder.typicode.com/posts/${postId}`)
     .then((res) => res.data);
 }
 
 export function createPostApi(newPost) {
-  return axios.post(`http://jsonplaceholder.typicode.com/posts`, newPost)
+  return axios.post(`https://jsonplaceholder.typicode.com/posts`, newPost)
     .then((res) => res.data);
 }
 
 export function editPostApi(postId, post) {
-  return axios.patch(`http://jsonplaceholder.typicode.com/posts/${postId}`, post)
+  return axios.patch(`https://jsonplaceholder.typicode.com/posts/${postId}`, post)
     .then((res) => res.data);
 }
